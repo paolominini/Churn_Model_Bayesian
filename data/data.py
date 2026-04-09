24b6df47e4bbc4330f1d241b094b217b7ccc8be1
Paolo-Minini <paolo.minini02@gmail.com>
2026-04-09 14:00:24 +0200

    data processing completed

--- data/data.py
+++ data/data.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 
 # --- PARAMS ---
-INPUT_FILE = "online_retail.csv"
+INPUT_FILE = "data/online_retail.csv"
 OUTPUT_X = "X.npy"
 OUTPUT_Y = "Y.npy"
 
 # --- LOADING ---
 print("Loading Data ...")
 df = pd.read_csv(INPUT_FILE, encoding="ISO-8859-1")
+df.columns = df.columns.str.replace(" ", "")
 
 df['CustomerID'] = pd.to_numeric(df['CustomerID'], errors= 'coerce')
 
@@ -16,10 +17,10 @@ df['CustomerID'] = pd.to_numeric(df['CustomerID'], errors= 'coerce')
 df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
 df = df.dropna(subset=['CustomerID'])
 df['CustomerID']=df['CustomerID'].astype(int)
-df = df[df['UnitPrice']>=0]
+df = df[df['Price']>=0]
 
-df['is_return']= df['InvoiceNo'].astype(str).str.upper().str.startswith('C')
-df['LineValue']= df['Quantity']*df['UnitPrice']
+df['is_return']= df['Invoice'].astype(str).str.upper().str.startswith('C')
+df['LineValue']= df['Quantity']*df['Price']
 
 global_last_date= df['InvoiceDate'].max()
 
@@ -36,7 +37,7 @@ last_purchase = (purchases.groupby('CustomerID')['InvoiceDate']
 recency = ((global_last_date - last_purchase).dt.days).rename('Recency')
 
 # --- Frequency ---
-frequency = (purchases.groupby('CustomerID')['InvoiceNo']
+frequency = (purchases.groupby('CustomerID')['Invoice']
              .nunique()
              .rename('Frequency'))
 
@@ -47,8 +48,8 @@ monetary = (purchases.groupby('CustomerID')['LineValue']
 log_monetary = np.log1p(monetary.clip(lower=0)).rename('Log_monetary_Value')
 
 # --- Cancellation Rate ---
-total_inv= df.groupby('CustomerID')['InvoiceNo'].nunique().rename('Total_Inv')
-cancel_inv = returns.groupby('CustomerID').nunique().rename("Cancel_Inv")
+total_inv= df.groupby('CustomerID')['Invoice'].nunique().rename('Total_Inv')
+cancel_inv = returns.groupby('CustomerID')['Invoice'].nunique().rename('Cancel_Inv')
 
 cancel_rate = (cancel_inv.div(total_inv, fill_value = 0)
                .fillna(0)
@@ -77,7 +78,7 @@ def std_gap(dates):
     diffs = s.diff().dropna().dt.days
     return diffs.std() if len(diffs) >= 1 else 0.0
 
-avg_time = (purchases.groupby("CustomerID")['InvoiceData']
+avg_time = (purchases.groupby("CustomerID")['InvoiceDate']
             .apply(std_gap)
             .rename('Avg_Time_Between_Purch')
             .fillna(0))
@@ -98,7 +99,7 @@ cs = pd.concat([
 
 # --- CHURN VARIABLE ---
 def mean_gap(dates):
-    s = dates.sort_values()
+    s = dates.dt.normalize().drop_duplicates().sort_values() # we drop duplicates for more orders in the same dat
     diffs = s.diff().dropna().dt.days
     return diffs.mean() if len(diffs) >= 1 else np.nan
 
@@ -108,10 +109,14 @@ mean_gap_per_customer = (purchases.groupby('CustomerID')['InvoiceDate']
 CHURN_WINDOW = int(mean_gap_per_customer.dropna().median())
 churn_cutoff = global_last_date - pd.Timedelta(days=CHURN_WINDOW)
 
+print(f"mean gap per customer: {mean_gap_per_customer.head()} days")
+print(f"Churn Window (median value of the mean gaps): {CHURN_WINDOW} days")
+print(f"Churn cutoff: {churn_cutoff}")
+
 cs['Churn'] = (cs['Last_Purchase_Date'] < churn_cutoff).astype(int)
 
 # --- OUTPUT NUMPY ARRAY ---
-FEATURE_COLS = ["Recency", "Frequency", "Log_Monetary_Value",
+FEATURE_COLS = ["Recency", "Frequency", "Log_monetary_Value",
     "Cancellation_Rate", "StockCode_Diversity", "Return_Propensity",
     "Avg_Time_Between_Purch", "Country_UK"]
 
@@ -119,4 +124,11 @@ X = cs[FEATURE_COLS].to_numpy(dtype=np.float64)
 y = cs["Churn"].to_numpy(dtype=np.int32)
 
 np.save(OUTPUT_X, X)
-np.save(OUTPUT_Y, Y)
+np.save(OUTPUT_Y, y)
+
+# -- RESULTS IN THE CHURN: IS THE DATASET BALANCED? --
+n_churn = y.sum()
+n_no_churn = len(y) - n_churn
+
+print(f"Churn (1): {n_churn} ({n_churn/len(y)})")
+print(f"Churn (0): {n_no_churn} ({n_no_churn/len(y)})")
